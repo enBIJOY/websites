af4e6bd30510be34bca9962ce4191a5f51d5e653
enBIJOY <162016650+enBIJOY@users.noreply.github.com>
2025-05-13 15:09:06 +0600

    first commit

---
+++ demo/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>demo</title>
+</head>
+<body>
+    <h2>
+        <?php
+            echo "Hello World";
+        ?>
+    </h2>
+</body>
+</html>
