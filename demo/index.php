ec85b13bff6e3ef1a83278d23f19a457049451cd
enBIJOY <162016650+enBIJOY@users.noreply.github.com>
2025-05-14 12:25:21 +0600

    Conditionals and boolean

--- demo/index.php
+++ demo/index.php
@@ -6,11 +6,11 @@
 <body>
     <h1>
         <?php
-<<<<<<< Updated upstream
+
             echo "Hello World";
             echo "Hi, Min sue woo";
             echo " My name is Bijoy";
-=======
+
         echo "My name is Bijoy <br>";
         echo "My". " ". "Pet <br>";
         
@@ -19,7 +19,24 @@
         echo "$name  <br>";
         echo "My"." "."$name  <br>";
         echo 'singale coted veriable $name <br>';
->>>>>>> Stashed changes
+        
+
+        // contionals and booleans
+
+        $classwork = "PHP Homework";
+        $work = FALSE;
+
+        if ($work) {
+            $message = "You did the $classwork";
+        } else {
+            $message = "You did NOT the $classwork";
+        }
+        ?>
+    </h1>
+
+    <h1>
+        <?php
+        echo $message;
         ?>
     </h1>
 
