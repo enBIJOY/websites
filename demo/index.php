392e67c822b55199f94e2bba5a57ad91ea539644
enBIJOY <162016650+enBIJOY@users.noreply.github.com>
2025-05-14 12:18:02 +0600

    Veriables and echo

--- demo/index.php
+++ demo/index.php
@@ -4,12 +4,24 @@
     <title>demo</title>
 </head>
 <body>
-    <h2>
+    <h1>
         <?php
+<<<<<<< Updated upstream
             echo "Hello World";
             echo "Hi, Min sue woo";
             echo " My name is Bijoy";
+=======
+        echo "My name is Bijoy <br>";
+        echo "My". " ". "Pet <br>";
+        
+        $name = "phone";
+
+        echo "$name  <br>";
+        echo "My"." "."$name  <br>";
+        echo 'singale coted veriable $name <br>';
+>>>>>>> Stashed changes
         ?>
-    </h2>
+    </h1>
+
 </body>
 </html>
