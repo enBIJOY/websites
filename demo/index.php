ef0379b1268dd666bd2d07f7c7896b7e0e1aaa46
enBIJOY <162016650+enBIJOY@users.noreply.github.com>
2025-05-14 12:01:26 +0600

    Conditionals and Booleans

--- demo/index.php
+++ demo/index.php
@@ -18,10 +18,41 @@
 
             echo '$pen'." "."not print as veriable <br>";
 
-            echo "$pen"." "."is not stolen";
+            echo "$pen"." "."is not stolen <br>";
 
 
+
+            // Conditionals and booleans
+
+            /*
+            $bookname = "PHP Book";
+            $read = TRUE;
+
+                if($read){
+                    $message = "You have read $bookname";
+                } else {
+                    $message = "You have NOT read $bookname";
+                }
+            */
+            
+
+            $Classwork = "Conditionals homework";
+            $work = FALSE;
+
+                if($work){
+                    $message = "You did the $Classwork";
+                } else{
+                    $message = "You did NOT the $Classwork";
+                }
         ?>
     </h2>
+    <h1>
+
+    <?php 
+    echo "$message"; 
+    //echo "$message2";
+    ?>
+
+    </h1>
 </body>
 </html>
