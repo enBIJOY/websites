107b72aa0d6d1a86964d2b586587bb08cf8c45c9
enBIJOY <162016650+enBIJOY@users.noreply.github.com>
2025-05-14 11:17:27 +0600

    veriable & echo

--- demo/index.php
+++ demo/index.php
@@ -6,9 +6,21 @@
 <body>
     <h2>
         <?php
-            echo "Hello World";
-            echo "Hi, Min sue woo";
-            echo " My name is Bijoy";
+
+            echo "concatinate"."my name is bijoy <br>";
+
+            echo 'another tex with single cout <br>';
+            echo "its double couted <br>";
+
+            $pen = "red pen";
+
+            echo "$pen";
+
+            echo '$pen'." "."not print as veriable <br>";
+
+            echo "$pen"." "."is not stolen";
+
+
         ?>
     </h2>
 </body>
